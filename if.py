f0be4602c8f02c3b6cd41bac1c7f4d42d88c360b
ShiroyashaD <167628990+ShiroyashaD@users.noreply.github.com>
2024-05-02 18:48:41 +0300

    Add files via upload

---
+++ if.py
@@ -0,0 +1,31 @@
+x = 38
+print('hello')
+if x < 0:
+    print('less then 0')
+print('goodbye')
+
+a, b = 10, 5
+if a > b and a > 0:
+    print('succsess')
+
+if (a > b) and (a > 0 or b < 1000):
+    print('succsess')
+
+if 5 < b and b < 10:
+     print('succsess')
+
+if '34' > '123':
+    print('succsess')
+
+if '123' > '12':
+    print('succsess')
+
+if [1, 2] > [1, 1]:
+    print('succsess')
+
+if '6' > 5:
+    print('succsess')
+if [5, 6] > 5:
+    print('succsess')
+if '6' != 5:
+    print('succsess')
